3e4a19d0625b8f1366cccbad2b08338fc1b60cc8
Math-ARod <matheusvwg@gmail.com>
2023-03-15 12:22:13 -0300

    created components and routes

--- frontend/portifolio/src/app/app-routing.module.ts
+++ frontend/portifolio/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ProjetosComponent } from './components/projetos/projetos.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'habilidades', component: HabilidadesComponent},
+  { path: 'projetos', component: ProjetosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/portifolio/src/app/app.module.ts
+++ frontend/portifolio/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
+import { ContatoComponent } from './components/contato/contato.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent,
+    HabilidadesComponent,
+    ProjetosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
